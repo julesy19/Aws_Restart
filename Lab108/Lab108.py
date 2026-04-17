739a1441cbbbb4b24061b81ff17ff7207015ca56
julesy19 <37917963+julesy19@users.noreply.github.com>
2026-04-17 11:39:33 -0400

    Deuxieme commit

---
+++ Lab108/Lab108.py
@@ -0,0 +1,4 @@
+print("Hello world")
+print("Mamadou Sy")
+
+
---
+++ Lab109/lab109.py
@@ -0,0 +1,62 @@
+print("Bonjour  Mamadou ")
+
+print("*" * 50)
+print("Lab 109")
+print("*" * 50)
+
+print("*" * 50)
+print("Exercice 2 : Présentation du type de données int")
+print("*" * 50)
+myValue=1
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("*" * 50)
+print("Exercice 3 : Présentation du type de données float")
+print("*" * 50)
+
+myValue=3.14
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+print("*" * 50)
+print("Exercice 3 : Présentation du type de données complex")
+print("*" * 50)
+
+myValue=5j
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+print("*" * 50)
+print("Exercice 3 : Présentation du type de données bool")
+print("*" * 50)
+
+myValue=True
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("*" * 50)
+print("*" * 50)
+
+print('Fin de Lab ')
+
+print("*" * 50)
+print("*" * 50)
